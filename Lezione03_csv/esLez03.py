ab4de9c75a6d8afda9d5ac8a40a17d6ccdf9760a
lorenzdeh <91345363+lorenzdeh@users.noreply.github.com>
2022-11-17 14:02:40 +0000

    cleaned up the code of file 'esLez03.py'

--- Lezione03_csv/esLez03.py
+++ Lezione03_csv/esLez03.py
@@ -6,13 +6,9 @@ def sum_csv(file_name):
         if elements[0] != 'Date':
             value = elements[1]
             #.strip('\n')
-            print('VALUE', value)
             if isfloat(value):
                 #Funziona ma dovrebbe esserci un altro modo per controllare se una variabile è float/int, ma da google al momento non trovo nulla che funzioni (isinstance(value, (float, int)), type(value) == float, non funzionano)
-                print('\t\tAFTER CHECK', value)
                 values.append(float(value))
-            else:
-                print('\tAFTER CHECK FALSE', value)
     file.close()
     if not values:
         return None
