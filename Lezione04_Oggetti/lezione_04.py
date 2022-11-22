b15e424f08cae65eebf87b58865b01d4440480a0
lorenzdeh <91345363+lorenzdeh@users.noreply.github.com>
2022-11-22 11:49:56 +0000

    wrote 'Lezione_04_Oggetti/lezione_04.py', tested it and it got  a 10, so all good

---
+++ Lezione04_Oggetti/lezione_04.py
@@ -0,0 +1,21 @@
+class CSVFile():
+    name = ""
+
+    def __init__(self, name):
+        self.name = name
+
+    def get_data(self):
+        val = []
+        file = open(self.name, 'r')
+        print('')
+        for item in file:
+            elements = item.split(',')
+            if elements[0] != 'Date':
+                elements[1] = elements[1].strip('\n')
+                val.append(elements)
+        file.close()
+        return val
+
+
+#file = CSVFile('shampoo_sales.csv')
+#print(file.get_data())
